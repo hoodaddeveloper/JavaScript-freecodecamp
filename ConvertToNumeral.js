3acaf4b1762c12fa1841562d424a2085856437c1
Mohammad Khafas <mohammadkhaffas@gmail.com>
2022-07-01 22:34:13 +0200

    Update ConvertToNumeral.js

--- ConvertToNumeral.js
+++ ConvertToNumeral.js
@@ -1,10 +1,11 @@
-/**
+/*
  * Convert the given number into a roman numeral.
  * All roman numerals answers should be provided in upper-case.
  *
  * @param num
  * @returns {string}
  */
+
 function convertToRoman(num) {
     let roman = "";
     let repeat;
